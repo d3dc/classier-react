0f8eb6f6822b7f933c6a67e89a2c8a6c9607a9bd
Sam Richard <sam.richard@gmail.com>
2018-08-24 22:06:20 -0600

    fix mapper getting minified wrong

--- src/mappers.js
+++ src/mappers.js
@@ -1,12 +1,10 @@
 import { config } from './config'
 
 export const tailwind = ({ variant, block, modifier, value }) =>
-  [
-    variant,
-    variant && config.join.variant,
-    block,
-    block && modifier && config.join.modifier,
-    modifier,
-    value && config.join.value,
-    value
-  ].join('')
+  (variant || '') +
+  ((variant && config.join.variant) || '') +
+  (block || '') +
+  ((block && modifier && config.join.modifier) || '') +
+  (modifier || '') +
+  ((value && config.join.value) || '') +
+  (value || '')
